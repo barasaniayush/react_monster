da18c5be7929502395e864790258a674358e401b
unknown <ayush.barasani@gmail.com>
2022-05-02 18:11:49 +0545

    Search function added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters : []
-    }
+      monsters : [],
+      searchField : ''
+    } 
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -17,12 +22,23 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value})
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonster = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className='App'>
-        {
-          this.state.monsters.map(monster => <h1 key={ monster.id }> { monster.name } </h1>)
-        }
+        <h1>Monster React App</h1>
+        <SearchBox
+          placeholder="Search monster here"
+          handleChange = { this.handleChange }  
+        />
+        <CardList monsters={filteredMonster} />
       </div>
     )
   }
